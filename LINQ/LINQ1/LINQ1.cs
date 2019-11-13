1511112b2f74589a953205a241964223942d9f10
Lahcen AIT TALEB <laittaleb@vente-privee.com>
2019-11-13 17:02:29 +0100

    first commuit

---
+++ LINQ/LINQ1/LINQ1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LINQ.LINQ1
+{
+    static class LINQ1
+    {
+        public static void WithoutLinq1(List<string> Names)
+        {
+            List<string> Results = new List<string>();
+
+            for (int i = 0; i < Names.Count; i++)
+            {
+                if (Names[i].Length>3) 
+                {
+                    Results.Add(Names[i]);
+                }
+            }
+            
+            foreach (var name in Results)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        public static void WithLinq_QuerySyntax(List<string> Names)
+        {
+           var Results = from item in Names where item.Length > 3 select item;
+            
+            foreach (var name in Results)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        public static void WithLinq_NonQuerySyntax(List<string> Names)
+        {
+            var Results = Names.Where(item => item.Length > 3);
+
+            foreach (var name in Results)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using LINQ.LINQ1;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            List<string> Names = new List<string>()
+            {
+                "lahcen","soukaina","khadija","bab","saida","imane","safae","tat"
+            };
+
+            Console.WriteLine("**************  Without LINQ *****************");
+            LINQ1.LINQ1.WithoutLinq1(Names);
+         
+
+            Console.WriteLine("**************  With LINQ Query Syntax *****************");
+            LINQ1.LINQ1.WithLinq_QuerySyntax(Names);
+
+            Console.WriteLine("**************  With LINQ Non Query Syntax *****************");
+            LINQ1.LINQ1.WithLinq_NonQuerySyntax(Names);
+
+
+            Console.ReadLine();
+        }
+    }
+}
