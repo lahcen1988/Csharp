569c0fff5e73324dc172246e1b2f2f6000b922ce
Lahcen AIT TALEB <laittaleb@vente-privee.com>
2019-11-20 13:55:20 +0100

    Permis

---
+++ LINQ/LINQ1/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace LINQ.LINQ1
+{
+    class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public Book()
+        {
+
+        }
+    }
+}
